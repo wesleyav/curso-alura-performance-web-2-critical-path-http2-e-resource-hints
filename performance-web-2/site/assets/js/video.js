d3ab6a619457c0385a29d1cd770a4e8efffb900c
Wesley <wesley.contato@hotmail.com>
2020-08-03 06:21:29 -0300

    Adiando a execução de um vídeo

---
+++ performance-web-2/site/assets/js/video.js
@@ -0,0 +1,7 @@
+setTimeout(function(){
+
+    var iframe = '<iframe class="elasticMedia" src="https://player.vimeo.com/video/99185150" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+
+    document.querySelector('.elasticMedia-container').innerHTML = iframe;
+
+}, 1200);
