68c398b7e9a801fbb447df05ebb21d5725807143
Wesley <wesley.contato@hotmail.com>
2020-08-03 07:10:59 -0300

    Throttle

--- performance-web-2/site/assets/js/lazyload.js
+++ performance-web-2/site/assets/js/lazyload.js
@@ -1,6 +1,14 @@
-window.onscroll = function () {
+var jarodei = false;
 
+window.onscroll = function () {
     // console.log('Scroll', scrollY); // log do scrool no console
+
+    //throttle
+    if (jarodei) return;
+    jarodei = true;
+    setTimeout(function () {
+        jarodei = false;
+    }, 100);
 
     var imgs = document.querySelectorAll('img[data-src]');
 
@@ -9,7 +17,5 @@ window.onscroll = function () {
         if (imgs[i].getBoundingClientRect().top < window.innerHeight + 200) {
             imgs[i].src = imgs[i].getAttribute('data-src')
         }
-
     }
-
 };
